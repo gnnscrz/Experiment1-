d0de8e2f507b40c17378683ca883818775a80dd2
gnnscrz <47804393+gnnscrz@users.noreply.github.com>
2019-03-13 12:15:10 +0800

    Add files via upload

---
+++ Experiment1-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	double m, d;
+	cout << "Mass in Grams: " << endl;
+	cin >> m;
+	cout << "Density in Grams/Cubic Centimeters" << endl;
+	cin >> d;
+	cout << "Volume" << M*4*D << endl;
+	
+	
+	
+	
+	
+	
+_getch();
+	return 0;
+}
